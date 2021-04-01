d404dafaff147d890097f2346bf2a2fb226c7a24
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-04-01 15:10:19 -0300

    Creating fragments

--- app/src/main/java/com/example/morsecodehero/MainActivity.kt
+++ app/src/main/java/com/example/morsecodehero/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.morsecodehero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.morsecodehero.ui.main.BarFragment
+import com.example.morsecodehero.ui.main.ButtonsFragment
+import com.example.morsecodehero.ui.main.LineFragment
 import com.example.morsecodehero.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container_bar, BarFragment.newInstance())
+                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container_line, LineFragment.newInstance())
+                .replace(R.id.container_buttons, ButtonsFragment.newInstance())
+                .commitNow()
         }
     }
 }
---
+++ app/src/main/java/com/example/morsecodehero/ui/main/BarFragment.kt
@@ -0,0 +1,32 @@
+package com.example.morsecodehero.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.morsecodehero.R
+
+class BarFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BarFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bar_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+}
