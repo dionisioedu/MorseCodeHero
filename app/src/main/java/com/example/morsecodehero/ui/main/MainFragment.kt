3a2792eabe138fd42ab8c0c10d46abfc7dea99bb
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-04-02 00:36:06 -0300

    Implements points and validation

--- app/src/main/java/com/example/morsecodehero/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/morsecodehero/ui/main/MainFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.example.morsecodehero.R
 
 class MainFragment : Fragment() {
@@ -14,17 +17,19 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        val textViewWord = view.findViewById<TextView>(R.id.textView_word)
+        textViewWord?.let { textView ->
+            viewModel.word.observe(viewLifecycleOwner, Observer {
+                textView.text = it ?: ""
+            })
+        }
 
+        return view
+    }
 }
